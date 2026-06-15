f26531690d24320b150abb7ab189b9990a11f2b8
Bhavana G N <bhavanagn13@gmail.com>
2026-06-15 18:43:41 +0530

    initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,37 @@
+pipeline{
+	agent any
+	tools{
+		maven 'Maven'
+	}
+	
+	stages {
+		stage('Build'){
+			steps{
+				sh 'mvn clean package'
+			}
+		}
+		stage('test'){
+			steps{
+				sh 'mvn test'
+		}
+		
+		stage('Run Application'){
+			steps{
+				sh 'mvn exec:java -Dexec.mainClass="com.example.App"'
+			}
+		}
+	}
+	
+	post{
+		sucess{
+			echo 'Build, test and execution successful'
+		}
+		failure{
+			echo 'Pipeline failed'
+		}
+		always{
+			junit 'target/surefire-reports/*.xml'
+		}
+	}
+}
+				
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,28 @@
+package com.example;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ImmutableList<String> list= ImmutableList.of("Apple" , "Bannana" , "Cherry");
+        System.out.println(list);
+        File source= new File("source.txt");
+        File dest=new File("dest.txt");
+        try{
+        	FileUtils.copyFile(source,dest);
+        	System.out.println("File copied successfully");
+        	
+            }
+       	catch(IOException e){
+       		System.out.println("File copying failed:"+e.getMessage());
+       	}
+}
+}
